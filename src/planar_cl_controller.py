6a9b86124507a1738c44842df020bd9688d1d313
Jarvis Schultz <jschultz@u.northwestern.edu>
2012-09-05 15:05:22 -0500

    getting closer to being able to test

--- src/planar_cl_controller.py
+++ src/planar_cl_controller.py
@@ -23,6 +23,9 @@ PUBLISHERS:
     - PlanarSystemConfig (filt_config)
     - RobotCommands     (serial_commands)
 
+SERVICES:
+    - Can reply with anything from the read-in trajectory (MUST ADD)
+
 NOTES:
     - Let's assume that the configurations that are received in
     this node are guaranteed to be in the right coordinate system.
@@ -45,7 +48,7 @@ import numpy as np
 import sys
 import scipy as sp
 import os
-
+import copy
 
 ## define some global constants:
 BALL_MASS = 0.1244 ## kg
@@ -224,13 +227,138 @@ class System:
         self.proc_cov = np.diag((0.1,0.1,0.1,0.1,0.15,0.15,0.15,0.15)) # process covariance
         self.est_cov = self.meas_cov # estimate covariance
 
-        # now we can define all callbacks, publishers, and subscribers:
+        # now we can define all callbacks, publishers, subscribers,
+        # services, and parameters:
         self.meas_sub = rospy.Subscriber("meas_config", PlanarSystemConfig, self.meascb)
         self.filt_pub = rospy.Publisher("filt_config", PlanarSystemConfig)
         self.comm_pub = rospy.Publisher("serial_commands", RobotCommands)
-        
+        if rospy.has_param("robot_index"):
+            self.robot_index = rospy.get_param("robot_index")
+        else:
+            self.robot_index = 0
+            rospy.set_param("robot_index")
+
+        # now let's initialize a bunch of variables that many of the
+        # other methods in this class will need access to
+        self.tbase = rospy.Time.from_sec(0)
+        self.k = int(0)
+        self.t1 = self.t2 = 0
+        self.u1 = np.zeros((self.systen.sys.nU, 1))
+        self.u2 =
+        self.Qmeas1 = np.zeros((self.system.sys.nQ, 1))
+        self.Qmeas2 = np.zeros((self.system.sys.nQ, 1))
+        self.Xmeas1 = np.zeros((self.system.dsys.nX, 1))
+        self.Xmeas2 = np.zeros((self.system.dsys.nX, 1))
+        self.Xpred1 = np.zeros((self.system.dsys.nX, 1))
+        self.Xpred2 = np.zeros((self.system.dsys.nX, 1))
+        self.Xest1 = np.zeros((self.system.dsys.nX, 1))
+        self.Xest2 = np.zeros((self.system.dsys.nX, 1))
+        # self.K1 = np.zeros((self.system.dsys.nU, self.system.dsys.nX))
+        # self.K2 = np.zeros((self.system.dsys.nU, self.system.dsys.nX))
+        # self.A1 = np.zeros((self.system.dsys.nX, self.system.dsys.nX))
+        # self.A2 = np.zeros((self.system.dsys.nX, self.system.dsys.nX))
+        # self.B1 = np.zeros((self.system.dsys.nX, self.system.dsys.nX))
+        # self.B2 = np.zeros((self.system.dsys.nX, self.system.dsys.nU))
+
         return
 
+
+    # def set_predefined_vars(self):
+    #     """
+    #     This function sets the status of all of the "1/2" variables
+    #     defined in init by just passing it an index for the location
+    #     in all of the arrays.
+    #     """
+
+    def copy_two_to_one(self):
+        """
+        This function merely performs a deep copy for all of the "2"
+        variables into the "1" variables.
+        """
+        self.t1 = copy.deepcopy(self.t2)
+        self.rho1 = copy.deepcopy(self.rho2)
+        self.Qmeas1 = copy.deepcopy(self.Qmeas2)
+        self.Xmeas1 = copy.deepcopy(self.Xmeas2)
+        self.Xpred1 = copy.deepcopy(self.Xpred2)
+        self.Xest1 = copy.deepcopy(self.Xest2)
+        return
+
+    def calc_send_controls(self):
+        self.u1 = self.Qref[self.k][2:]
+        self.u2 = self.Uref[self.k] + \
+          matmult(self.Kproj[self.k], self.Xref[self.k]-self.Xest2)
+        # now convert to a velocity and send out:
+        ucom = (u2-u1)/DT
+        com = RobotCommands()
+        com.robot_index = self.robot_index
+        com.type = 'n'
+        com.v_left = ucom[0]
+        com.v_right = ucom[0]
+        com.v_top = 0
+        com.v_top_left = ucom[1]
+        com.v_top_right = 0
+        com.div = 3
+        self.comm_pub.publish(com)
+
+        return
+
+    def stop_robot(self):
+        com = RobotCommands()
+        com.robot_index = self.robot_index
+        
+
+    # define the callback function for the estimate out of the kinect
+    def meascb(self, data):
+        rospy.logdebug("measurement callback triggered")
+
+        # first, let's get the operating condition and act
+        # appropriately:
+        op = rospy.get_param("/operating_condition")
+
+        if (op != 2):
+            # we are not running, so let's just keep running the
+            # initializations and exiting
+            first_flag = True
+            return
+
+        if first_flag:
+            # this is the first run through the callback while in the
+            # running state, so let's initialize all of the variables,
+            # and then make sure to set the flag back to false at the
+            # end of the callback
+            self.tbase = data.header.stamp
+            self.t2 = self.tbase.to_sec()
+            self.k = 0
+            # fill out X2 stuff:
+            self.Qmeas2 = np.array([data.xm, data.ym, data.xr, data.r])
+            # let's assume that both the discrete generalized momenta
+            # and the kinematic configuration variables are zero
+            # because we are at rest at this point
+            self.Xmeas2 = np.hstack((self.Qmeas2, np.zeros(self.system.sys.nQ)))
+            # our initial prediction for the state is just the initial
+            # state of the system:
+            self.Xpred2 = self.Xref[self.k]
+            # for our initial estimate, let's just average the
+            # prediction and the measurement
+            self.Xest2 = (self.Xmes2+self.Xpred2)/2.0
+            # now run our control law
+            self.calc_send_controls()
+            # send out nominal (feedforward) controls:
+            first_flag = False
+            return
+
+        # let's increment our index:
+        self.k += 1
+        # do we need to exit?
+
+        # copy X2 stuff to X1:
+
+        # calculate and fill out new X2 stuff:
+
+        # run control law and send controls:
+
+
+        return
 
 
 def main():
